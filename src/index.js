5f73fce84555d343da935cef6ca4f6f137ce0564
Narender Singh <68217842+techiesnarender@users.noreply.github.com>
2023-05-11 14:27:04 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path: "login",
+    path: "signin",
     element: <SignIn />,
   },
   {
--- src/routes/SignIn.jsx
+++ src/routes/SignIn.jsx
@@ -1,19 +1,18 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import {
   FormControl,
   IconButton,
@@ -21,6 +20,8 @@ import {
   InputLabel,
   OutlinedInput,
 } from "@mui/material";
+import { useForm } from "react-hook-form";
+import AuthService from "../services/auth.service";
 
 function Copyright(props) {
   return (
@@ -43,13 +44,50 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   console.log({
+  //     email: data.get("email"),
+  //     password: data.get("password"),
+  //   });
+  // };
+
+  let navigate = useNavigate();
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(4, "Password must be at least 4 characters")
+      .max(40, "Password must not exceed 40 characters"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(JSON.stringify(data, null, 2));
+    AuthService.login(data.email, data.password).then(
+      () => {
+        navigate("/");
+        window.location.reload();
+      },
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        console.log(resMessage);
+      }
+    );
   };
 
   const [showPassword, setShowPassword] = React.useState(false);
@@ -64,6 +102,7 @@ export default function SignIn() {
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
+        {/* <form onSubmit={handleSubmit(onSubmit)}> */}
         <Box
           sx={{
             marginTop: 8,
@@ -78,29 +117,38 @@ export default function SignIn() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
+
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <FormControl
+              variant="standard"
+              onSubmit={handleSubmit(onSubmit)}
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <FormControl sx={{ m: 1 }} variant="outlined" fullWidth>
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                {...register("email")}
+                className={`form-control ${errors.email ? "is-invalid" : ""}`}
+              />
+            </FormControl>
+            <div className="invalid-feedback">{errors.email?.message}</div>
+            <FormControl sx={{ marginTop: 2 }} variant="outlined" fullWidth>
               <InputLabel htmlFor="outlined-adornment-password">
-                Password
+                Password *
               </InputLabel>
               <OutlinedInput
                 id="outlined-adornment-password"
                 required
+                {...register("password")}
+                className={`form-control ${
+                  errors.password ? "is-invalid" : ""
+                }`}
                 type={showPassword ? "text" : "password"}
                 endAdornment={
                   <InputAdornment position="end">
@@ -117,6 +165,7 @@ export default function SignIn() {
                 label="Password"
               />
             </FormControl>
+            <div className="invalid-feedback">{errors.password?.message}</div>
 
             {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
@@ -138,12 +187,14 @@ export default function SignIn() {
               </Grid>
               <Grid item>
                 <Link to={"/signout"} component={RouterLink} variant="body2">
+                  <br />
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
+        {/* </form> */}
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
